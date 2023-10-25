7c11af79010c0b251f33bbada948ea899d66ce14
triplespamlegend <145274624+triplespamlegend@users.noreply.github.com>
2023-10-25 18:27:11 -0500

    Update list.js

--- js/list.js
+++ js/list.js
@@ -5,6 +5,26 @@ const d = {
 			"vids": [
 				{
 					"user": "nucleus",
+					"link": "https://youtu.be/nLRyt1HPfpk?si=Tyt49OCaulQhgGHG",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+      ],
+			"name": "Septuple Spam Legend",
+			"author": "doublespamnoob",
+			"more": "none",
+			"id": "test",
+			"pass": "N/A",
+			"percentToQualify": "100",
+			"verificationVid": "https://youtu.be/nLRyt1HPfpk?si=Tyt49OCaulQhgGHG",
+/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 0
+		},
+				{
+			"vids": [
+				{
+					"user": "nucleus",
 					"link": "https://youtu.be/r3ASjqAW90Y?si=VRd9T1acAYwxPrVp",
 					"percent": 100,
 					"hz": "60hz"
