87204cb3daaba0b4d1961ef5a5d0dc1048ae4598
triplespamlegend <145274624+triplespamlegend@users.noreply.github.com>
2023-10-27 13:14:43 -0500

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -104,7 +104,7 @@ function getUserData(user) {
     Swal.fire({
         title : "#"+(user+1)+" : "+user_data[user].name,
         html : '<center><strong>Score : '+user_data[user].point + '<br>'+
-            'Best Record : '+user_data[user].highest+'<br>'+
+            'Best Completion : '+user_data[user].highest+'<br>'+
             'Completed Levels : '+clears+' Level(s)<br>'+
             '<br>Record List : '+progresses+'<br>'+
             '</strong></center>'
