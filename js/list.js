bcefa505c03ee465c977e8706e28219c8a74738f
triplespamlegend <145274624+triplespamlegend@users.noreply.github.com>
2023-10-29 10:11:37 -0500

    Update list.js

--- js/list.js
+++ js/list.js
@@ -89,7 +89,24 @@ const d = {
 					"percent": 100,
 					"hz": "60hz"
 				},
-
+{
+					"user": "WhizKid05",
+					"link": "https://www.youtube.com/watch?v=e4pdHjWuZJQ",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "me !",
+					"link": "https://youtu.be/sDwNW2vShqc?si=ozLW_PBqk8wvB4K2",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Ray131",
+					"link": "https://youtu.be/XbL8sksT73c?si=dE2SiD1U2Lgnzibj",
+					"percent": 100,
+					"hz": "60hz"
+				},
       ],
 			"name": "Quadruple Spam Legend",
 			"author": "JoinMolten",
@@ -109,7 +126,54 @@ const d = {
 					"percent": 100,
 					"hz": "60hz"
 				},
-
+{
+					"user": "me !",
+					"link": "https://youtu.be/jW61Hzd_Ubo?si=aRkr6pOzKE8PiPBL",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Ray131",
+					"link": "https://youtu.be/VFlIXQntdyc?si=7k-C8VkU54qN4kaQ",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "NXISE",
+					"link": "https://youtu.be/XZy5MJWSDcc?si=d2ls7WY2bIKDN340",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "loooookkk",
+					"link": "https://youtu.be/KU0yvlQEu-o?si=uPcZd09-pJnbuUv3",
+					"percent": 100,
+					"hz": "60hz"
+				},
+					{
+					"user": "WhizKid05",
+					"link": "https://youtu.be/xgL_u9YELTs?si=vVeekkaIWvuxvnOs",
+					"percent": 100,
+					"hz": "60hz"
+				},
+					{
+					"user": "PaperGuy",
+					"link": "https://youtu.be/EyW5px0M280?si=1JsuQXoD03TG5sAO",
+					"percent": 100,
+					"hz": "60hz"
+				},
+						{
+					"user": "Tector9",
+					"link": "https://youtu.be/Jx9xxtUvKy8?si=VT9mBUS_hPCSdAu-",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "EmpanadR",
+					"link": "https://youtu.be/RGbDiqnqyoQ?si=C8dS-sgdgI1xuJKl",
+					"percent": 100,
+					"hz": "60hz"
+				},
       ],
 			"name": "Triple Spam Legend",
 			"author": "BensonGD",
@@ -118,6 +182,109 @@ const d = {
 			"pass": "N/A",
 			"percentToQualify": "100",
 			"verificationVid": "https://www.youtube.com/watch?v=xQ9pcsiS2Ak",
+/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 0
+		},
+				{
+			"vids": [
+				{
+					"user": "nucleus",
+					"link": "https://youtu.be/smp9sJMhZzA?si=LXyUcnk0__L67ObO",
+					"percent": 100,
+					"hz": "60hz"
+				},
+		{
+					"user": "WildPoint",
+					"link": "https://youtu.be/yVOsDWYnLJk?si=muFuXD1wsPVjQtsu",
+					"percent": 100,
+					"hz": "60hz"
+				},
+						{
+					"user": "Fenyahks",
+					"link": "https://youtu.be/rO2e4g4q4LU?si=48hOsZPq0PZ-tPuQ",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Ray131",
+					"link": "https://youtu.be/NQs56YEtIgM?si=ajdFt28wR76DWTTy",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+						"user": "MeowPau",
+					"link": "https://youtu.be/DVdcX3yBSUU?si=4k2q2QCBZC14wH4F",
+					"percent": 100,
+					"hz": "60hz"
+				},
+					{
+						"user": "Epictuber",
+					"link": "https://youtu.be/ZjnWusDios0?si=aFGcTnRqxvi6YDPX",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+						"user": "Cybermoth",
+					"link": "https://youtu.be/ZIGrQbKLll8?si=Sn63zmtnaUB3YHe0",
+					"percent": 100,
+					"hz": "60hz"
+				},
+					{
+						"user": "Nintenfox",
+					"link": "https://youtu.be/w_ArjWG80Sg?si=Euy_I9SJf5IyWnoJ",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+						"user": "KodiaX",
+					"link": "https://youtu.be/E196Co28gJQ?si=Og_LQ22ny130Gw-U",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+						"user": "U3dpZnRZ",
+					"link": "https://youtu.be/E196Co28gJQ?si=Og_LQ22ny130Gw-U",
+					"percent": 100,
+					"hz": "60hz"
+				},
+						{
+						"user": "JeDoesStuff",
+					"link": "https://youtu.be/lBYnwpYI7xU?si=8SV_H0DG3ijxzImw",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+						"user": "Pixel",
+					"link": "https://youtu.be/og7cQGNd46E?si=x9dCNeitJFPeQm62",
+					"percent": 100,
+					"hz": "60hz"
+				},
+							{
+						"user": "PPPixel",
+					"link": "https://youtu.be/d1hw_hvFHpg?si=hEu47HS5HpPDxYB1",
+					"percent": 100,
+					"hz": "60hz"
+				},
+											{
+						"user": "PaperGuy",
+					"link": "https://youtu.be/1D8lD-81uyY?si=UnA8I-WskhyUlQ63",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+						"user": "EmpanadR",
+					"link": "https://youtu.be/8A852z6I51Q?si=xPFX3r7-Wagfh0dt",
+					"percent": 100,
+					"hz": "60hz"
+				},
+      ],
+			"name": "Spam Legend",
+			"author": "nxise",
+			"more": "none",
+			"id": "test",
+			"pass": "N/A",
+			"percentToQualify": "100",
+			"verificationVid": "https://youtu.be/DDfu9CDqf-g?si=1L9BTtbW04DSt7kh",
 /*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
 			"key": 0
 		},
